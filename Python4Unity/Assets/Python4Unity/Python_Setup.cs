639d59d7da3080b27e92811b45290ff63fb2812e
k4sima_0 <44926913+k4sima@users.noreply.github.com>
2022-01-10 15:17:33 +0900

    init

---
+++ Python4Unity/Assets/Python4Unity/Python_Setup.cs
@@ -0,0 +1,38 @@
+using Python.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Python_Setup : MonoBehaviour
+{
+	IEnumerator Start()
+	{
+		Runtime.PythonDLL = Application.dataPath + "/StreamingAssets/Python4Unity/python39/python39.dll";
+		PythonEngine.Initialize(mode: ShutdownMode.Reload);
+
+		IEnumerator coroutine = ver();
+		yield return coroutine;
+		Debug.Log("Hello Python! : " + coroutine.Current);
+	}
+
+	public void OnApplicationQuit()
+	{
+		if (PythonEngine.IsInitialized)
+		{
+			PythonEngine.Shutdown(ShutdownMode.Reload);
+			Debug.Log("Python ShutDowned");
+		}
+	}
+
+	public IEnumerator ver()
+	{
+		yield return new WaitUntil(() => PythonEngine.IsInitialized);
+		using (Py.GIL())
+		{
+			dynamic sys = Py.Import("sys");
+			dynamic platform = Py.Import("platform");
+
+			yield return "Python" + sys.version + "\nPlatform:" + platform.platform(terse: true) + "\nUnity:" + Application.unityVersion;
+		}
+	}
+}
